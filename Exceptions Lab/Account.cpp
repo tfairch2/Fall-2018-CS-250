345a76e03efabcf2010fcf1fe1893547d9be8d8e
Terry Fairchild <tfairch2@users.noreply.github.com>
2018-09-29 16:43:29 -0500

    Add files via upload

--- Exceptions Lab/Account.cpp
+++ Exceptions Lab/Account.cpp
@@ -2,6 +2,25 @@
 #include <string.h>
 using namespace std;
 
+
+
+//************************ STEP 3 ************************
+struct InsufficientFunds : public exception {
+public:
+	InsufficientFunds() {}
+	virtual const char* what() const throw() {
+		return "\nError, account has insufficient funds for withdrawl!\n";
+	}
+};
+double withdraw(double amount) {
+	if (amount > balance) {
+		throw InsufficientFunds();
+	}
+}
+//************************end of step 3 **********************
+
+
+
 //************************ STEP 4 ************************
 struct NegativeAmount: public exception{
 	public:
@@ -11,6 +30,27 @@ struct NegativeAmount: public exception{
 		}
 };
 //*******************end of Step 4 ***********************
+
+
+//**************************** Step 5 ************************
+struct NegativeAmountWithdrawn : public exception {
+public:
+	NegativeAmountWithdrawn() {}
+	virtual const char* what() const throw() {
+		return "\nError, you can't withdraw a negative amount!\n";
+	}
+};
+
+double withdrawNegative(double amount) {
+	if (amount < 0) {
+		throw NegativeAmountWithdrawn();
+	}
+	//*********************** end of Step 5*********************
+	else {
+		balance -= amount;
+		return balance;
+	}
+}
 
 
 class Account
@@ -30,44 +70,5 @@ class Account
 		double getBalance(){
 			return balance;
 		}
-	
-//************************ STEP 3 ************************
-		struct InsufficientFunds : public exception {
-		public:
-			InsufficientFunds() {}
-			virtual const char* what() const throw() {
-				return "\nError, account has insufficient funds for withdrawl!\n";
-			}
-		};
-		double withdraw(double amount) {
-			if (amount > balance) {
-				throw InsufficientFunds();
-			}
-		}
-//************************end of step 3 **********************
 
-
-
-
-
-//**************************** Step 5 ************************
-		struct NegativeAmountWithdrawn : public exception {
-		public:
-			NegativeAmountWithdrawn() {}
-			virtual const char* what() const throw() {
-				return "\nError, you can't withdraw a negative amount!\n";
-			}
-		};
-
-		double withdrawNegative(double amount) {
-				if (amount < 0) {
-					throw NegativeAmountWithdrawn();
-				}				
-//*********************** end of Step 5*********************
-
-				else {
-					balance -= amount;
-					return balance;
-				}
-		}
 };
